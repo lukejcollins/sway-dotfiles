bdbf72a69bfe3f937e168e65e9a8744b7c9d2422
Luke Collins <luke.collins@live.co.uk>
2023-06-25 17:49:02 +0100

    Altered swaylock dependency on script

--- .local/bin/swaylock-on-sleep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dbus-monitor --system "type='signal',interface='org.freedesktop.login1.Manager',member='PrepareForSleep'" |
-while read -r line; do
-    if echo $line | grep "true" &> /dev/null; then
-        swaymsg exec swaylock
-    fi
-done
